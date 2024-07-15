069c6b2cfe0a1bf28bd7a808fb138258a5614c7b
Ryan Tusi <ryantusi45@gmail.com>
2024-07-16 00:50:00 +0530

    preloader

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,9 @@ jQuery(document).ready(function( $ ) {
 
   // Preloader
   $(window).on('load', function() {
-    $('#preloader').delay(100).fadeOut('slow',function(){$(this).remove();});
+    $('#preloader').delay(100).fadeOut('slow', function() {
+      $(this).remove();
+    });
   });
 
   // Hero rotating texts
